3b52687903cd0fa13e823e52179656541b3ff8d0
Karl Benbeneck <kbenbeneck@gmail.com>
2018-11-05 02:35:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,8 @@ def reverse_each_word(sentence)
   sentence = sentence.split.each { |n| n.reverse! }
   sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+  sentence = sentence.split.collect { |n| n.reverse! }
+  sentence.join(" ")
+end
